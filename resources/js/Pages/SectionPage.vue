b6bea3d2f37defc952fdbf021abf961ce93e5ff5
katoj65 <katoj652gmail.com>
2023-07-26 23:54:17 +0300

    Added a new section dialog box

--- resources/js/Pages/SectionPage.vue
+++ resources/js/Pages/SectionPage.vue
@@ -50,17 +50,104 @@
 </div>
 
 
-
-
-
-
-
-
-
-
-
-
 </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<form class=""  v-if="show==true" style="position:fixed;width:100%;left:0;top:0;z-index:10000;height:100%;background-color: hsla(210, 29%, 18%, 0.3);">
+    <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header" style="background: #37BEA7;border:none;">
+                    <h5 class="modal-title" style="color:white;">Reference</h5>
+                    <a href="#" class="close" data-dismiss="modal" aria-label="Close" @click="show=false">
+
+                    </a>
+                </div>
+                <div class="modal-body" style="max-height:500px;overflow:auto">
+
+
+
+
+
+<div class="form-group">
+    <label class="form-label" for="default-01"></label>
+    <div class="form-control-wrap">
+        <input type="text" class="form-control" id="default-01" placeholder="Enter names">
+    </div>
+</div>
+
+
+
+
+<div class="form-group">
+    <label class="form-label" for="default-01"></label>
+    <div class="form-control-wrap">
+        <input type="text" class="form-control" id="default-01" placeholder="Enter address">
+    </div>
+</div>
+
+
+
+<div class="form-group">
+    <label class="form-label" for="default-01"></label>
+    <div class="form-control-wrap">
+        <input type="text" class="form-control" id="default-01" placeholder="Enter telephone contact">
+    </div>
+</div>
+
+
+
+<div class="form-group">
+    <label class="form-label" for="default-01"></label>
+    <div class="form-control-wrap">
+        <input type="text" class="form-control" id="default-01" placeholder="Enter email address">
+    </div>
+</div>
+
+<div class="form-group">
+    <label class="form-label" for="default-01"></label>
+    <div class="form-control-wrap">
+        <input type="text" class="form-control" id="default-01" placeholder="Enter position">
+    </div>
+</div>
+
+
+
+
+
+
+
+
+                </div>
+                <div class="modal-footer bg-light">
+                    <span class="sub-text">
+
+
+<input type="submit" class="button" value="Save" style="border-radius:10px"/>
+
+
+
+                    </span>
+                </div>
+            </div>
+        </div>
+</form>
 </app-layout>
 </template>
 <script>
@@ -72,13 +159,30 @@ AppLayout
 
 props:{
 title:{},
-response:{}
+response:{},
+errors:{},
+flash:{}
 },
 
 computed:{
 section(){
 return this.response.section;
 }
+
+
+
+},
+
+data(){return{
+show:false,
+show2:false,
+
+
+}
+},
+
+methods:{
+
 
 
 
---
+++ resources/js/Section/AddSectionForm.vue
@@ -0,0 +1,43 @@
+<template>
+
+
+</template>
+<script>
+export default {
+    components:{},
+    props:{
+    pages:{},
+    errors:{},
+    flash:{}
+    },
+    data(){return{
+    show:false,
+
+
+
+
+
+
+
+
+    }},
+
+
+    methods:{
+
+
+
+
+
+    },
+
+    computed:{
+
+
+
+
+
+        
+    }
+}
+</script>
